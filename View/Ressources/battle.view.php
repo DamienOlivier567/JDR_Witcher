3bacaa0d1690e0cb6dd8d28a2c9806daebe4aa71
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-06-03 14:29:25 +0200

    Battlemaps et Mapmonde fini

--- View/Ressources/battle.view.php
+++ View/Ressources/battle.view.php
@@ -1,1 +1,84 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/351e9300a0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../assets/css/charactercreation.css">
+    <title>BattleMaps</title>
+</head>
+<body>
 <?php
+require_once "../_Partials/header.view.php"
+?>
+
+<div class="row">
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/battlemaps/kaer_morhen.png">
+                        <img class="img-fluid" src="../../fichiers/resources/battlemaps/kaer_morhen.png" alt="classe"></a>
+                </div>
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Kaer Morhen</h3>
+                    <p>
+                        Une Battlemap de Kaer Morhen
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/battlemaps/kaer_morhen.png" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/battlemaps/temple_lilvani.png">
+                        <img class="img-fluid" src="../../fichiers/resources/battlemaps/temple_lilvani.png" alt="carnet"></a>
+                </div>
+
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Temple de Lilvani</h3>
+                    <p>
+                        Le temple où Gaunter demande à Geralt de faire venir Olgier.
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/battlemaps/temple_lilvani.png" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/battlemaps/white_orchard.jpg">
+                        <img class="img-fluid" src="../../fichiers/resources/battlemaps/white_orchard.jpg" alt=""></a>
+                </div>
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Cimetière de White Orchard</h3>
+                    <p>
+                        Battlemap du cimetière de White Orchand de The Witcher 3
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/battlemaps/white_orchard.jpg" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+</body>
+</html>
+
+
+
+
--- View/Ressources/mapp.view.php
+++ View/Ressources/mapp.view.php
@@ -1,1 +1,103 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/351e9300a0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../assets/css/charactercreation.css">
+    <title>Mappemonde</title>
+</head>
+<body>
 <?php
+require_once "../_Partials/header.view.php"
+?>
+
+<div class="row">
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/mappemonde/monde.jpg">
+                        <img class="img-fluid" src="../../fichiers/resources/mappemonde/monde.jpg" alt="classe"></a>
+                </div>
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Carte Du Monde</h3>
+                    <p>
+                        Une carte du monde de The Witcher en assez bonne qualité (15mo).
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/mappemonde/monde.jpg" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/mappemonde/monde2.jpg">
+                        <img class="img-fluid" src="../../fichiers/resources/mappemonde/monde2.jpg" alt="carnet"></a>
+                </div>
+
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Carte Du Monde</h3>
+                    <p>
+                        Une carte du monde de The Witcher
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/mappemonde/monde2.jpg" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/mappemonde/skellige_v1.png">
+                        <img class="img-fluid" src="../../fichiers/resources/mappemonde/skellige_v1.png" alt=""></a>
+                </div>
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Skellige</h3>
+                    <p>
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/mappemonde/skellige_v1.png" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
+        <!-- DOCUMENT -->
+        <div class="mb-5 mb-md-4">
+            <div class="row">
+                <div class="col-md-4 order-2 order-md-1">
+                    <a target='blank'  href="../../fichiers/resources/mappemonde/toussaint.pdf">
+                        <img class="img-fluid" src="../../fichiers/resources/mappemonde/tousaint.PNG" alt=""></a>
+                </div>
+                <div class="col-md col-md order-1 order-md-2">
+                    <h3>Toussaint</h3>
+                    <p>
+
+                    </p>
+                    <p><a target='blank' href="../../fichiers/resources/mappemonde/toussaint.pdf" class="btn btn-sm btn-danger"><i class="fas fa-download"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
